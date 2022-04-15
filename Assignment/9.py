9c83695bd393ac5a46781fad92c95c8de3314411
DevNakum <ndev2003@gmail.com>
2022-04-15 16:47:39 +0530

    assignment

--- Assignment/9.py
+++ Assignment/9.py
@@ -1,10 +1,3 @@
-# Write a function named move_rectangle that takes a Rectangle and two numbers
-# named dx and dy. It should change the location of the rectangle by adding 
-# dx to the x coordinate of corner and adding dy to the y coordinate of corner.
-
-# Current Status: Complete
-
-
 class Point(object):
     """Represents a point in 2d space"""
 
